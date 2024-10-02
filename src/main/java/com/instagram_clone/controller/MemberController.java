f57521bb7e0d6f1950c6cbdade3745e8bb8aa3cb
Lee-Backend-Developer <eotmd1016@gmail.com>
2024-10-02 16:49:11 +0900

    수정: 컨트롤러 api로 수정하고 Json으로 값을 받을수 있게 하였음
    vue 프론트에서 json으로 값을 보내는데 백단에서 값을 받을수 없는 현상이 있었다

--- src/main/java/com/instagram_clone/controller/MemberController.java
+++ src/main/java/com/instagram_clone/controller/MemberController.java
@@ -6,20 +6,24 @@ import com.instagram_clone.request.member.SignUpForm;
 import com.instagram_clone.response.Response;
 import com.instagram_clone.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("member")
+@RequestMapping("/api/member")
 public class MemberController {
     private final MemberService memberService;
 
     // 로그인
     @PostMapping("login")
-    public ResponseEntity<Response> login(LoginForm loginForm) {
+    public ResponseEntity<Response> login(@RequestBody LoginForm loginForm) {
+        log.info("loginForm: {}", loginForm);
         Member login = memberService.login(loginForm);
         return ResponseEntity.ok(Response.builder()
                 .state("success")
@@ -30,7 +34,7 @@ public class MemberController {
     }
     // 회원가입
     @PostMapping("signUp")
-    public ResponseEntity<Response> register(SignUpForm signUpForm) {
+    public ResponseEntity<Response> register(@RequestBody SignUpForm signUpForm) {
         Member register = memberService.signUp(signUpForm);
         return ResponseEntity.ok(Response.builder()
                 .state("success")
